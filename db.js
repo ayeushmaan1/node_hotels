848b2b488e1e41e0ca8703dc8820bc1e35727c66
Ayushmaan Dwivedi <ayushmaandwivediiaf@gmail.com>
2025-09-10 08:33:40 +0530

    env error resolved

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose =require('mongoose');
-
+require('dotenv').config();
 //const mongoURL='mongodb://localhost:27017/hotels';
-const mongoURL='mongodb+srv://ayushmaandwivediiaf_db_user:nk1cAkyNuSLqPsM8@cluster0.pirsypm.mongodb.net/';
+const mongoURL=process.env.MONGODB_URL;
 mongoose.connect(mongoURL,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
