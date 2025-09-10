1bd9d460e4d294c79a1f1d19e37da3557a7f24eb
Ayushmaan Dwivedi <ayushmaandwivediiaf@gmail.com>
2025-09-10 08:14:55 +0530

    .en upar kia

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 const express=require ('express')
 const db=require('./db');
 const app = express();
-
-const PORT=process.env.PORT||3000;
 require('dotenv').config();
+const PORT=process.env.PORT||3000;
+
   
 
 const bodyParser = require('body-parser');
